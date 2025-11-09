dca835025ce2453ef406b05cb476832f2ff4dfe6
Taha-Saleem-43 <tahasaleem981@gmail.com>
2025-11-09 18:54:49 +0500

    Added the functionality

--- src/execute.c
+++ src/execute.c
@@ -10,11 +10,11 @@ int execute(char* arglist[]) {
             exit(1);
         case 0: // Child process
             execvp(arglist[0], arglist);
-            perror("Command not found"); // This line runs only if execvp fails
+            perror("Command not found");
             exit(1);
         default: // Parent process
             waitpid(cpid, &status, 0);
-            // printf("Child pid:%d exited with status %d\n", cpid, status >> 8);
             return 0;
     }
 }
+
