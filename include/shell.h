74289fd4916c4db10a5c007f0d3f2d0b1ff7a5ed
Taha-Saleem-43 <tahasaleem981@gmail.com>
2025-11-09 21:19:21 +0500

    Added the error no library

--- include/shell.h
+++ include/shell.h
@@ -3,12 +3,14 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
 #include <ctype.h>  // for isdigit()
+#include <errno.h>
 
 //Additional External Libraries
 #include <readline/readline.h>
