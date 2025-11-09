b7624035ee7551b66c90c70750079a5c4a284a87
Taha-Saleem-43 <tahasaleem981@gmail.com>
2025-11-09 19:25:34 +0500

    Added the helper function declartion and some libraries

--- include/shell.h
+++ include/shell.h
@@ -8,16 +8,27 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <ctype.h>  // for isdigit()
 
 #define MAX_LEN 512
 #define MAXARGS 10
 #define ARGLEN 30
 #define PROMPT "FCIT> "
 
+// --------- History support ---------
+#define HISTORY_SIZE 20
+extern char* history[HISTORY_SIZE];  // shared array for last commands
+extern int history_count;            // number of commands stored
+
 // Function prototypes
 char* read_cmd(char* prompt, FILE* fp);
 char** tokenize(char* cmdline);
 int execute(char** arglist);
-int handle_builtin(char** args); 
+int handle_builtin(char** args);
+
+// History-related functions
+void add_to_history(const char* cmdline);
+int handle_bang_command(char** cmdline_ptr);
 
 #endif // SHELL_H
+
